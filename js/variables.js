71bd6ce1aff87752f78b3218e7bc9376eb56a5f6
aarongsmz-dev <aarongsmz@gmail.com>
2023-08-24 13:15:52 +0100

    udemy

---
+++ js/variables.js
@@ -0,0 +1,20 @@
+// Variables
+
+const tweet = "Aprendiendo Javascript con el curso"
+
+const producto = "Monitor HD"
+
+//Length para contar cuantas palabras tiene
+
+console.log(tweet.length);
+
+//IndexOf (retorna la posición)
+
+console.log(tweet.indexOf('JavaScript'));
+
+// Includes (retorna true o false)
+
+console.log(tweet.includes('JavaScript'));
+
+const numero1 = 100;
+const numero2 = 200; 
